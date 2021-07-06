4c233c8e443e59e044c15d2abf818cd78ea36e21
Sławomir Brzozowski <brzozasr@interia.eu>
2021-07-06 09:40:03 +0200

    Code refactoring.

--- Services/ForecastServices/PointWeatherForecastService.cs
+++ Services/ForecastServices/PointWeatherForecastService.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using WeatherAPI.Extensions;
-using WeatherAPI.Models;
 using WeatherAPI.Models.PointWeatherForecast;
 using WeatherAPI.Models.WeatherForecast;
 
@@ -25,139 +24,175 @@ namespace WeatherAPI.Services.ForecastServices
 
             if (weatherForecast is not null)
             {
-                List<WeatherPwf> weatherCurrList = null;
-
-                if (weatherForecast.CurrentWf.Weathers is not null)
-                {
-                    weatherCurrList = new List<WeatherPwf>();
-                    
-                    foreach (var weather in weatherForecast.CurrentWf.Weathers)
-                    {
-                        var weatherCurr = new WeatherPwf
-                        {
-                            Id = weather.Id,
-                            Main = weather.Main,
-                            Description = weather.Description.FirstLetterToUpper(),
-                            Icon = weather.Icon
-                        };
-                    
-                        weatherCurrList.Add(weatherCurr);
-                    }
-                }
-
-                RainPwf rainCurr = null;
-                
-                if (weatherForecast.CurrentWf.Rain is not null)
-                {
-                    rainCurr = new RainPwf
-                    {
-                        H1 = weatherForecast.CurrentWf.Rain.H1
-                    };
-                }
-                
-                SnowPwf snowCurr = null;
-                
-                if (weatherForecast.CurrentWf.Snow is not null)
-                {
-                    snowCurr = new SnowPwf
-                    {
-                        H1 = weatherForecast.CurrentWf.Snow.H1
-                    };
-                }
-
-                List<MinutelyPwf> minutely = null;
-
-                if (weatherForecast.Minutely is not null)
-                {
-                    minutely = new List<MinutelyPwf>();
-                    
-                    foreach (var min in weatherForecast.Minutely)
-                    {
-                        var minute = new MinutelyPwf
-                        {
-                            DtLocal = DateTimeOffset.FromUnixTimeSeconds(
-                                min.Dt + weatherForecast.TimezoneOffset).DateTime,
-                            Precipitation = min.Precipitation
-                        };
-                    
-                        minutely.Add(minute);
-                    }
-                }
-                
-                List<HourlyPwf> hourly = null;
-
-                if (weatherForecast.Hourly is not null)
-                {
-                    foreach (var hour in weatherForecast.Hourly)
-                    {
-                        hourly = new List<HourlyPwf>();
-                    
-                        var hourlyPwf = new HourlyPwf
-                        {
-                            DtLocal = DateTimeOffset.FromUnixTimeSeconds(
-                                hour.Dt + weatherForecast.TimezoneOffset).DateTime,
-                            Temp = hour.Temp,
-                            FeelsLike = hour.FeelsLike,
-                            Pressure = hour.Pressure,
-                            Humidity = hour.Humidity,
-                            DewPoint = hour.DewPoint,
-                            Uvi = hour.Uvi,
-                            Clouds = hour.Clouds,
-                            Visibility = hour.Visibility,
-                            WindSpeed = hour.WindSpeed,
-                            WindDeg = hour.WindDeg,
-                            WindGust = hour.WindGust
-                        };
-                    
-                        hourly.Add(hourlyPwf);
-                    }
-                }
-
                 var point = new PointWeatherForecast
                 {
                     Lat = weatherForecast.Lat,
                     Lon = weatherForecast.Lon,
                     Timezone = weatherForecast.Timezone,
                     TimezoneOffset = weatherForecast.TimezoneOffset,
-                    Current = new CurrentPwf
-                    {
-                        DtUtc = DateTimeOffset.FromUnixTimeSeconds(
-                            weatherForecast.CurrentWf.Dt).DateTime,
-                        DtLocal = DateTimeOffset.FromUnixTimeSeconds(
-                            weatherForecast.CurrentWf.Dt + weatherForecast.TimezoneOffset).DateTime,
-                        SunriseLocal = DateTimeOffset.FromUnixTimeSeconds(
-                            weatherForecast.CurrentWf.Sunrise + weatherForecast.TimezoneOffset).DateTime,
-                        SunsetLocal = DateTimeOffset.FromUnixTimeSeconds(
-                            weatherForecast.CurrentWf.Sunset + weatherForecast.TimezoneOffset).DateTime,
-                        Temp = weatherForecast.CurrentWf.Temp,
-                        FeelsLike = weatherForecast.CurrentWf.FeelsLike,
-                        Pressure = weatherForecast.CurrentWf.Pressure,
-                        Humidity = weatherForecast.CurrentWf.Humidity,
-                        DewPoint = weatherForecast.CurrentWf.DewPoint,
-                        Uvi = weatherForecast.CurrentWf.Uvi,
-                        Clouds = weatherForecast.CurrentWf.Clouds,
-                        VisibilityKm = (float?) Math.Round(weatherForecast.CurrentWf.Visibility / 1000.0f, 1,
-                            MidpointRounding.ToZero),
-                        WindSpeedKmPerH = (float?) Math.Round(
-                            weatherForecast.CurrentWf.WindSpeed * 3.6f, 1, MidpointRounding.ToZero),
-                        WindDir = new WindDir
-                        {
-                            DirTxt = weatherForecast.CurrentWf.WindDeg.DirectionTxt(),
-                            DirArrow = weatherForecast.CurrentWf.WindDeg.DirectionArrow()
-                        },
-                        WindGustKmPerH = (float?) Math.Round(
-                            weatherForecast.CurrentWf.WindGust ?? 0 * 3.6f, 1, MidpointRounding.ToZero) == 0
-                            ? null : (float?) Math.Round(
-                                weatherForecast.CurrentWf.WindGust ?? 0 * 3.6f, 1, MidpointRounding.ToZero),
-                        Weathers = weatherCurrList,
-                        Rain = rainCurr,
-                        Snow = snowCurr,
-                    },
-                    Minutely = minutely,
-                    Hourly = hourly,
+                    Current = GetCurrent(weatherForecast),
+                    Minutely = GetMinutely(weatherForecast),
+                    Hourly = GetHourly(weatherForecast),
                 };
 
                 return point;
+            }
+
+            return null;
+        }
+
+        private CurrentPwf GetCurrent(WeatherForecast weather)
+        {
+            if (weather.CurrentWf is not null)
+            {
+                var current = new CurrentPwf
+                {
+                    DtUtc = DateTimeOffset.FromUnixTimeSeconds(
+                        weather.CurrentWf.Dt).DateTime,
+                    DtLocal = DateTimeOffset.FromUnixTimeSeconds(
+                        weather.CurrentWf.Dt + weather.TimezoneOffset).DateTime,
+                    SunriseLocal = DateTimeOffset.FromUnixTimeSeconds(
+                        weather.CurrentWf.Sunrise + weather.TimezoneOffset).DateTime,
+                    SunsetLocal = DateTimeOffset.FromUnixTimeSeconds(
+                        weather.CurrentWf.Sunset + weather.TimezoneOffset).DateTime,
+                    Temp = weather.CurrentWf.Temp,
+                    FeelsLike = weather.CurrentWf.FeelsLike,
+                    Pressure = weather.CurrentWf.Pressure,
+                    Humidity = weather.CurrentWf.Humidity,
+                    DewPoint = weather.CurrentWf.DewPoint,
+                    Uvi = weather.CurrentWf.Uvi,
+                    Clouds = weather.CurrentWf.Clouds,
+                    VisibilityKm = (float?) Math.Round(weather.CurrentWf.Visibility / 1000.0f, 1,
+                        MidpointRounding.ToZero),
+                    WindSpeedKmPerH = (float?) Math.Round(
+                        weather.CurrentWf.WindSpeed * 3.6f, 1, MidpointRounding.ToZero),
+                    WindDir = new WindDir
+                    {
+                        DirTxt = weather.CurrentWf.WindDeg.DirectionTxt(),
+                        DirArrow = weather.CurrentWf.WindDeg.DirectionArrow()
+                    },
+                    WindGustKmPerH = (float?) Math.Round(
+                        weather.CurrentWf.WindGust ?? 0 * 3.6f, 1, MidpointRounding.ToZero) == 0
+                        ? null
+                        : (float?) Math.Round(
+                            weather.CurrentWf.WindGust ?? 0 * 3.6f, 1, MidpointRounding.ToZero),
+                    Weathers = GetWeather(weather.CurrentWf.Weathers),
+                    Rain = GetRain(weather.CurrentWf.Rain),
+                    Snow = GetSnow(weather.CurrentWf.Snow),
+                };
+
+                return current;
+            }
+
+            return null;
+        }
+
+        private IEnumerable<MinutelyPwf> GetMinutely(WeatherForecast weather)
+        {
+            List<MinutelyPwf> minutely = null;
+
+            if (weather.Minutely is not null)
+            {
+                minutely = new List<MinutelyPwf>();
+                    
+                foreach (var min in weather.Minutely)
+                {
+                    var minute = new MinutelyPwf
+                    {
+                        DtLocal = DateTimeOffset.FromUnixTimeSeconds(
+                            min.Dt + weather.TimezoneOffset).DateTime,
+                        Precipitation = min.Precipitation
+                    };
+                    
+                    minutely.Add(minute);
+                }
+            }
+
+            return minutely;
+        }
+
+        private IEnumerable<HourlyPwf> GetHourly(WeatherForecast weather)
+        {
+            List<HourlyPwf> hourly = null;
+
+            if (weather.Hourly is not null)
+            {
+                foreach (var hour in weather.Hourly)
+                {
+                    hourly = new List<HourlyPwf>();
+                    
+                    var hourlyPwf = new HourlyPwf
+                    {
+                        DtLocal = DateTimeOffset.FromUnixTimeSeconds(
+                            hour.Dt + weather.TimezoneOffset).DateTime,
+                        Temp = hour.Temp,
+                        FeelsLike = hour.FeelsLike,
+                        Pressure = hour.Pressure,
+                        Humidity = hour.Humidity,
+                        DewPoint = hour.DewPoint,
+                        Uvi = hour.Uvi,
+                        Clouds = hour.Clouds,
+                        Visibility = hour.Visibility,
+                        WindSpeed = hour.WindSpeed,
+                        WindDeg = hour.WindDeg,
+                        WindGust = hour.WindGust
+                    };
+                    
+                    hourly.Add(hourlyPwf);
+                }
+            }
+
+            return hourly;
+        }
+
+        private IEnumerable<WeatherPwf> GetWeather(IEnumerable<WeatherWf> weatherWf)
+        {
+            if (weatherWf is not null)
+            {
+                var weatherList = new List<WeatherPwf>();
+                    
+                foreach (var weather in weatherWf)
+                {
+                    var weatherCurr = new WeatherPwf
+                    {
+                        Id = weather.Id,
+                        Main = weather.Main,
+                        Description = weather.Description.FirstLetterToUpper(),
+                        Icon = weather.Icon
+                    };
+                    
+                    weatherList.Add(weatherCurr);
+                }
+
+                return weatherList;
+            }
+
+            return null;
+        }
+
+        private RainPwf GetRain(RainWf rainWf)
+        {
+            if (rainWf is not null)
+            {
+                var rain = new RainPwf
+                {
+                    H1 = rainWf.H1
+                };
+                return rain;
+            }
+
+            return null;
+        }
+
+        private SnowPwf GetSnow(SnowWf snowWf)
+        {
+            if (snowWf is not null)
+            {
+                var snow = new SnowPwf
+                {
+                    H1 = snowWf.H1
+                };
+                return snow;
             }
 
             return null;
